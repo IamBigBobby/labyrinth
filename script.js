2237882f60a5661552144037066c0ddad03990c7
Denis Svetleishii <denissvetleishii@outlook.com>
2022-11-14 20:39:01 +0400

    feat: add game area

---
+++ script.js
@@ -0,0 +1,59 @@
+
+const data = `{
+	"width": 10,
+	"height": 10,
+	"fields": [
+		{"x": 2, "y": 2, "type": "wall"},
+		{"x": 3, "y": 2, "type": "wall"},
+		{"x": 4, "y": 2, "type": "wall"},
+		{"x": 5, "y": 2, "type": "wall"},
+		{"x": 6, "y": 2, "type": "wall"},
+		{"x": 8, "y": 1, "type": "wall"},
+		{"x": 9, "y": 3, "type": "wall"},
+		{"x": 10, "y": 3, "type": "wall"},
+		{"x": 2, "y": 3, "type": "wall"},
+		{"x": 2, "y": 4, "type": "wall"},
+		{"x": 2, "y": 5, "type": "wall"},
+		{"x": 5, "y": 4, "type": "wall"},
+		{"x": 6, "y": 4, "type": "wall"},
+		{"x": 7, "y": 4, "type": "wall"},
+		{"x": 8, "y": 4, "type": "wall"},
+		{"x": 9, "y": 4, "type": "wall"},
+		{"x": 5, "y": 5, "type": "wall"},
+		{"x": 5, "y": 6, "type": "wall"},
+		{"x": 5, "y": 7, "type": "wall"},
+		{"x": 2, "y": 7, "type": "wall"},
+		{"x": 2, "y": 8, "type": "wall"},
+		{"x": 2, "y": 9, "type": "wall"},
+		{"x": 3, "y": 9, "type": "wall"},
+		{"x": 4, "y": 9, "type": "wall"},
+		{"x": 5, "y": 9, "type": "wall"},
+		{"x": 9, "y": 6, "type": "wall"},
+		{"x": 9, "y": 7, "type": "wall"},
+		{"x": 9, "y": 8, "type": "wall"},
+		{"x": 9, "y": 9, "type": "wall"},
+		{"x": 7, "y": 7, "type": "wall"},
+		{"x": 7, "y": 8, "type": "wall"},
+		{"x": 7, "y": 9, "type": "wall"},
+		{"x": 7, "y": 10, "type": "wall"},
+		{"x": 1, "y": 1, "type": "enter"},
+		{"x": 9, "y": 5, "type": "exit"}
+	]
+}`;
+
+const parsedData = JSON.parse(data);
+
+console.log(parsedData.width);
+
+let table = document.querySelector('#table');
+ 
+for (let i = 0; i < parsedData.width; i++) {
+    let tr = document.createElement('tr');
+    
+    for (let i = 0; i < parsedData.height; i++) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    
+    table.appendChild(tr);
+}
